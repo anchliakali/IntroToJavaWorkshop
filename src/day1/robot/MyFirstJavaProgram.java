0a9704a694a378a627a859460a987e0d9296f52f
is8bella <league@WTS5.attlocal.net>
2016-06-28 14:58:18 -0700

    we learned how to make and command robots.

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -5,11 +5,34 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
-	}
+		// START HERE
+		Robot Kinder = new Robot();
+        Kinder.penDown();
+        Kinder.setPenWidth(100);
+		Kinder.setPenColor(0, 230, 238);
+		Kinder.move(500);
+		Kinder.turn(90);
+		Kinder.setPenColor(0, 138,221);
+		Kinder.move(500);
+		Kinder.turn(90);
+		Kinder.move(500);
+		Kinder.turn(90);
+	    Kinder.move(500);
+
+	    
+	    for (int i = 0; i < args.length; i++) {
+	    Kinder.move(100);
+	    Kinder.turn(90);
 }
+	}		
+}
+
+
+
+
+
+
+
+
+
--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,27 +10,31 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot Zoom = new Robot();
+		Zoom.hide();
 		// 3. Put the robot's pen down
-
+		Zoom.penDown();
 		// 8. Make the robot go at maximum speed (10)
-
+		Zoom.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
+		Zoom.setPenColor(Color.CYAN);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+		int sides = 5;
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+		int turn = 360/sides;
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
-
+		for (int i = 0; i<999999999; i++) {
+			
+		
 			// 2. Move the robot 200 pixels
-	
+		Zoom.move(i);
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
-	
+		
 			// 6. Turn the robot the amount in your angle variable
-	
+		Zoom.turn(turn);
 			// 11. Turn the robot one more degree
-
+		Zoom.turn(1);
+		}
 	}
 
 	// Variations:
