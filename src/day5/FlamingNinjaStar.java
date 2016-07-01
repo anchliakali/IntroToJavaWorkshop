c568140bae5e13dab9fbd6fd46d06e362b90d534
is8bella <league@WTS5.attlocal.net>
2016-07-01 14:57:58 -0700

    SC SC Scary

--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -17,26 +17,27 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+		Robot Line = new Robot();
+		Line.penDown();
 		// 12. Set the robot speed to 10
-
+		Line.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		Line.turn(1/8);
 			// 3. Move the robot 64 pixels
-
+		Line.move(64);
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			Line.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			Line.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			Line.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			Line.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			Line.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
 			
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
